e36fb968331e516c2e663cf54acb067b8dcf4e6f
Timo Kloss <timo@inutilis.com>
2015-04-16 12:42:49 +0200

    new launch animation.

--- Pixels/Pixels/SplashViewController.m
+++ Pixels/Pixels/SplashViewController.m
@@ -7,10 +7,16 @@
 //
 
 #import "SplashViewController.h"
+#import "AudioPlayer.h"
 
 @interface SplashViewController ()
 
 @property (weak, nonatomic) IBOutlet UIImageView *logoImageView;
+@property (weak, nonatomic) IBOutlet UILabel *label;
+
+@property BOOL animationDone;
+@property BOOL timerDone;
+@property AudioPlayer *audioPlayer;
 
 @end
 
@@ -20,21 +26,87 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    UIWindow* window = [UIApplication sharedApplication].windows.firstObject;
+    
+    self.label.transform = CGAffineTransformMakeTranslation(0, window.frame.size.height * -0.5f - 35.0f - 12.0f);
+    self.logoImageView.transform = CGAffineTransformMakeTranslation(0, 20);
+    self.logoImageView.alpha = 0.0f;
+    
+    self.audioPlayer = [[AudioPlayer alloc] init];
 }
 
 - (void)viewDidAppear:(BOOL)animated
 {
     [super viewDidAppear:animated];
     
-    [UIView animateWithDuration:0.5 delay:0.3 options:0 animations:^{
+    [NSTimer scheduledTimerWithTimeInterval:4 target:self selector:@selector(onTimerComplete:) userInfo:nil repeats:NO];
+    [self.audioPlayer start];
+    
+    [UIView animateWithDuration:1 delay:0.3 options:UIViewAnimationOptionCurveLinear animations:^{
         
-        self.logoImageView.transform = CGAffineTransformMakeRotation(M_PI_2);
+        self.label.transform = CGAffineTransformMakeTranslation(0, 0);
         
     } completion:^(BOOL finished) {
         
-        [self showApp];
+        [UIView animateWithDuration:0.5 delay:0.0 options:UIViewAnimationOptionCurveLinear animations:^{
+
+            self.logoImageView.transform = CGAffineTransformMakeTranslation(0, 0);
+            self.logoImageView.alpha = 1.0f;
+            [self playSound];
+            
+        } completion:^(BOOL finished) {
+            
+            self.animationDone = YES;
+            [self checkComplete];
+        }];
         
     }];
+}
+
+- (BOOL)prefersStatusBarHidden
+{
+    return YES;
+}
+
+- (void)playSound
+{
+    SoundNote *note;
+    
+    note = [self.audioPlayer nextNoteForVoice:0];
+    note->pitch = 52;
+    note->duration = 6;
+    
+    note = [self.audioPlayer nextNoteForVoice:0];
+    note->pitch = 50;
+    note->duration = 6;
+    
+    note = [self.audioPlayer nextNoteForVoice:1];
+    note->pitch = 48;
+    note->duration = 6;
+
+    note = [self.audioPlayer nextNoteForVoice:1];
+    note->pitch = 46;
+    note->duration = 6;
+}
+
+- (void)viewDidDisappear:(BOOL)animated
+{
+    [self.audioPlayer stop];
+}
+
+- (void)onTimerComplete:(id)sender
+{
+    self.timerDone = YES;
+    [self checkComplete];
+}
+
+- (void)checkComplete
+{
+    if (self.animationDone && self.timerDone)
+    {
+        [self showApp];
+    }
 }
 
 - (void)showApp
