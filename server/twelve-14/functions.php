f60a7271bafb750d0dfdf288ea539b9cbbd54687
Timo Kloss <timo@inutilis.com>
2015-08-16 18:21:13 +0200

    web community: open graph tags and images.

--- server/twelve-14/functions.php
+++ server/twelve-14/functions.php
@@ -1,10 +1,147 @@
 <?php
+
+require 'autoload.php';
  
+use Parse\ParseClient;
+use Parse\ParseQuery;
+use Parse\ParseException;
+use Parse\ParseUser;
+
+define("LCC_IMAGE_WIDTH", 640);
+define("LCC_IMAGE_HEIGHT", 360);
+
+$lccPostId = $_GET["lccpost"];
+$lccUserId = $_GET["lccuser"];
+$lccPost = NULL;
+$lccUser = NULL;
+$lccUserPosts = NULL;
+
+
 // Unregister 3 Periodic Footer Widget Sidebars
 function twelve14_remove_sidebar() {
       unregister_sidebar( 'sidebar-1' ); 
 }
 add_action( 'widgets_init', 'twelve14_remove_sidebar', 11 );
 
+
+function get_safe_string($string) {
+	$clear = preg_replace('/ +/', ' ', preg_replace('/[\\"\n]/', ' ', strip_tags($string)));
+	if (strlen($clear) > 300) {
+		$clear = substr($clear, 0, 300) . "...";
+	}
+	return $clear;
+}
+
+function community_title($title, $sep, $seplocation) {
+	global $lccPost, $lccUser, $lccUserId;
+
+	if ($lccPost) {
+		return get_safe_string($lccPost->get("title")) . " " . $sep . " ";
+	}
+	if ($lccUser && !empty($lccUserId)) {
+		return get_safe_string($lccUser->get("username")) . " " . $sep . " ";
+	}
+
+    return $title;
+}
+add_filter('wp_title', 'community_title', 10, 3);
+
+
+function get_lcc_large_image_url($post) {
+	$name = "/lccimages/post_" . $post->getObjectId() . ".png";
+	$filePath = WP_CONTENT_DIR . $name;
+	$fileUrl = WP_CONTENT_URL . $name;
+
+	if (!file_exists($filePath)) {
+		$srcImageFile = file_get_contents($post->get("image")->getURL());
+
+		$dstImage = imagecreatetruecolor(LCC_IMAGE_WIDTH, LCC_IMAGE_HEIGHT);
+		$srcImage = imagecreatefromstring($srcImageFile);
+
+		$srcSize = getimagesizefromstring($srcImageFile);
+		$srcWidth = $srcSize[0];
+		$srcHeight = $srcSize[1];
+
+		imagecopyresized($dstImage, $srcImage, 0, 0, 0, 0, LCC_IMAGE_WIDTH, LCC_IMAGE_HEIGHT, $srcWidth, $srcHeight);
+
+		imagepng($dstImage, $filePath);
+	}
+
+	return $fileUrl;
+}
+
+function community_init() {
+	global $lccPostId, $lccUserId, $lccPost, $lccUser, $lccUserPosts;
+
+	if (basename(get_page_template()) == 'community.php') {
+		ParseClient::initialize('JjXUGeQFrN79s4TcIunronsM13ehsBy0Pa1FLIUA', 'YlUL5qFSyRFVS0rFLdIHUQjNuTzHi294XDy7G9Em', 'g8mqpMyqSxN78OUfwmT7gwPLqOIQ4VqG4N0YWAg3');
+
+		try {
+			if (!empty($lccPostId)) {
+
+				$query = new ParseQuery("Post");
+				$query->includeKey("user");
+				$lccPost = $query->get($lccPostId);
+				$lccUser = $lccPost->get("user");
+
+			} else {
+
+				if (!empty($lccUserId)) {
+					// User
+					$query = ParseUser::query();
+					$lccUser = $query->get($lccUserId);
+				} else {
+					// "LowRes Coder" account
+					$query = ParseUser::query();
+					$lccUser = $query->get("T5VWaLW28x");
+				}
+
+				$query = new ParseQuery("Post");
+				$query->equalTo("user", $lccUser);
+				$query->notEqualTo("type", 2);
+				$query->includeKey("sharedPost");
+				$query->descending("createdAt");
+				$lccUserPosts = $query->find();
+
+			}
+
+		} catch (ParseException $ex) {
+			
+		}
+	}
+}
+add_action('wp', 'community_init');
+
+
+remove_action('wp_head', 'rel_canonical');
+remove_action('wp_head', 'wp_shortlink_wp_head');
+
+function insert_fb_in_head() {
+	global $post, $lccPost, $lccUser;
+    if ( !is_singular()) //if it is not a post or a page
+        return;
+
+    if ($lccPost) {
+    	$title = get_safe_string($lccPost->get("title"));
+    	$detail = get_safe_string($lccPost->get("detail"));
+		echo '<meta property="og:title" content="' . $title . '"/>';
+		echo '<meta property="og:description" content="' . $detail . '"/>';
+		echo '<meta property="og:type" content="article"/>';
+		echo '<meta property="og:url" content="' . get_permalink() . "?" . $_SERVER['QUERY_STRING'] . '"/>';
+		echo '<meta property="og:image" content="' . get_lcc_large_image_url($lccPost) . '"/>';
+		echo '<meta property="og:image:width" content="' . LCC_IMAGE_WIDTH . '"/>';
+		echo '<meta property="og:image:height" content="' . LCC_IMAGE_HEIGHT . '"/>';
+    } else {
+		echo '<meta property="og:title" content="' . get_the_title() . '"/>';
+		echo '<meta property="og:description" content="Program your own retro games or demos directly on your iPad or iPhone!"/>';
+		echo '<meta property="og:type" content="article"/>';
+		echo '<meta property="og:url" content="' . get_permalink() . '"/>';
+		echo '<meta property="og:image" content="http://lowres.inutilis.com/wordpress/wp-content/uploads/2015/08/shareimage.png"/>';
+	}
+	echo '<meta property="og:site_name" content="LowRes Coder"/>';
+    echo "
+";
+}
+add_action('wp_head', 'insert_fb_in_head');
 
 ?>
--- server/twelve-14/page-templates/community.php
+++ server/twelve-14/page-templates/community.php
@@ -7,60 +7,28 @@
  * @since Twelve_Fourteen 1.1
  */
 
-require dirname(__FILE__) . '/../autoload.php';
- 
-use Parse\ParseClient;
-use Parse\ParseQuery;
-use Parse\ParseException;
-use Parse\ParseUser;
-
-$lccPostId = $_GET["lcc_post_id"];
-$lccUserId = $_GET["lcc_user_id"];
-
 $pageUrl = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-
-ParseClient::initialize('JjXUGeQFrN79s4TcIunronsM13ehsBy0Pa1FLIUA', 'YlUL5qFSyRFVS0rFLdIHUQjNuTzHi294XDy7G9Em', 'g8mqpMyqSxN78OUfwmT7gwPLqOIQ4VqG4N0YWAg3');
-
-$lccPost = NULL;
-$lccUser = NULL;
 
 $title = "";
 
-try {
-	if (!empty($lccPostId)) {
-
-		$query = new ParseQuery("Post");
-		$query->includeKey("user");
-		$lccPost = $query->get($lccPostId);
-		$lccUser = $lccPost->get("user");
-
+if (!empty($lccPostId)) {
+	if ($lccPost) {
 		$title = $lccPost->get("title");
-
 	} else {
-
-		if (!empty($lccUserId)) {
-			// User
-			$query = ParseUser::query();
-			$lccUser = $query->get($lccUserId);
+		$title = "Program not found";
+	}
+} else {
+	if (!empty($lccUserId)) {
+		// User
+		if ($lccUser) {
 			$title = $lccUser->get("username");
 		} else {
-			// "LowRes Coder" account
-			$query = ParseUser::query();
-			$lccUser = $query->get("T5VWaLW28x");
-			$title = "Featured Programs";
+			$title = "User not found";
 		}
-
-		$query = new ParseQuery("Post");
-		$query->equalTo("user", $lccUser);
-		$query->notEqualTo("type", 2);
-		$query->includeKey("sharedPost");
-		$query->descending("createdAt");
-		$lccUserPosts = $query->find();
-
+	} else {
+		// "LowRes Coder" account
+		$title = "Featured Programs";
 	}
-
-} catch (ParseException $ex) {
-	$title = "Not found";
 }
 
 get_header(); ?>
@@ -82,7 +50,7 @@ get_header(); ?>
 		<?php if ($lccPost) { ?>
 
 		<?php
-			$postImageUrl = $lccPost->get("image")->getURL();
+			$postImageUrl = get_lcc_large_image_url($lccPost);
 			$programFile = $lccPost->get("programFile");
 			if ($programFile) {
 				$sourceCodeUrl = $programFile->getURL();
@@ -91,9 +59,10 @@ get_header(); ?>
 			}
 		?>
 
+		<p class="lcc-author"><strong>By <a href="<?php echo $pageUrl . "?lccuser=" . $lccUser->getObjectId(); ?>"><?php echo $lccUser->get("username"); ?></a></strong></p>
 		<div class="lcc-images"><img src="<?php echo $postImageUrl; ?>"></div>
-		<p class="lcc-author">By <a href="<?php echo $pageUrl . "?lcc_user_id=" . $lccUser->getObjectId(); ?>"><?php echo $lccUser->get("username"); ?></a></p>
 		<p class="lcc-description"><?php echo wptexturize(nl2br($lccPost->get("detail"))); ?></p>
+		<p><strong><a href="https://itunes.apple.com/us/app/lowres-coder/id962117496?mt=8&uo=4">Get LowRes Coder</a> to use this program.</strong></p>
 
 		<iframe class="lcc-sourcecode" src="<?php echo $sourceCodeUrl; ?>" width="100%" height="400"></iframe>
 
@@ -114,7 +83,7 @@ get_header(); ?>
  		?>
 
  				<div class="lcc-post">
- 					<a href="<?php echo $pageUrl . "?lcc_post_id=" . $userPost->getObjectId(); ?>">
+ 					<a href="<?php echo $pageUrl . "?lccpost=" . $userPost->getObjectId(); ?>">
  						<img src="<?php echo $userPost->get("image")->getURL(); ?>">
  						<p><?php echo $userPost->get("title"); ?></p>
  					</a>
